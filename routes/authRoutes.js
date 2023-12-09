b958258b209b9e8b44d7854f70f173281d6a9cea
SudipBaidyadev <139113892+SudipBaidyadev@users.noreply.github.com>
2023-12-09 18:13:39 +0530

    origin

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const { signup, signin, logout, userProfile } = require('../controllers/authController');
 // const { isAuthenticated } = require('../middleware/auth');
+const cors = require('cors');
 
+router.use(cors());
 
 //auth routes
 // /api/signup
--- routes/helplineRoutes.js
+++ routes/helplineRoutes.js
@@ -3,17 +3,17 @@ const router = express.Router();
 // const { createJob, singleJob, updateJob, showJobs } = require('../controllers/jobsController');
 const { isAuthenticated, isAdmin } = require('../middleware/auth');
 const { createHelpline, singleHelpline, updateHelpline, showHelplines } = require('../controllers/helplineController');
+const cors = require('cors');
 
-
-
+router.use(cors());
 //helplines routes
 
 // /api/helpline/create
-router.post('/helpline/create', isAuthenticated, isAdmin, createHelpline);
+router.post('/helpline/create', isAuthenticated, createHelpline);
 // /api/helpline/id
 router.get('/helpline/:id', singleHelpline);
 // /api/helpline/update/helpline_id
-router.put('/helpline/update/:helpline_id', isAuthenticated, isAdmin, updateHelpline);
+router.put('/helpline/update/:helpline_id', isAuthenticated,  updateHelpline);
 // // /api/helplines/show
 router.get('/helplines/show', showHelplines);
 
